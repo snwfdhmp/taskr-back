0073daf50485bc3fc189a67a4f0dd0ccb5cea2df
Martin Joly <snwfdhmp@gmail.com>
2018-02-18 16:09:44 +0100

    use of golang package to parse webhooks

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/rjz/githubhook.v0"
 )
 
 var (
@@ -16,25 +17,33 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/webhook", func(rw http.ResponseWriter, req *http.Request) {
-		log.Infoln("Trigger /webhook")
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(req.Body)
-		log.Println(buf.String())
+		secret := []byte("don't tell!")
+		hook, err := githubhook.Parse(secret, req)
+		if err != nil {
+			fmt.Println("fatal:", err)
+			return
+		}
+		log.Println(hook)
 	})
 
 	r.HandleFunc("/callback", func(rw http.ResponseWriter, req *http.Request) {
 		log.Infoln("Trigger /callback")
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(req.Body)
-		log.Println(buf.String())
+		print(req)
 	})
 
 	r.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
 		log.Infoln("Trigger /")
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(req.Body)
-		log.Println(buf.String())
+		print(req)
 	})
 
 	panic(http.ListenAndServe(":9876", r))
+}
+
+func print(req *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	log.Println(buf.String())
+	log.Println(req.Header.Get("x-hub-signature"))
+	log.Println(req.Header.Get("x-github-event"))
+	log.Println(req.Header.Get("x-github-delivery"))
 }
