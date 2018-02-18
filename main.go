9a72b017115df8e17355559a44611572f4a8c0c2
Martin Joly <snwfdhmp@gmail.com>
2018-02-18 16:58:21 +0100

    handle issueEvent

--- main.go
+++ main.go
@@ -46,8 +46,8 @@ func main() {
 			issue := data.GetIssue().GetTitle()
 			comment := data.GetComment().GetBody()
 			log.Printf("New comment '%s' by %s on issue '%s' on repo %s", comment, name, issue, repo)
-		} else if _, ok := payload.(*github.IssueComment); ok {
-			log.Println("is IssueComment")
+		} else if _, ok := payload.(*github.IssueEvent); ok {
+			log.Println("is IssueEvent")
 		} else if _, ok := payload.(*github.User); ok {
 			log.Println("is User")
 		} else {
